d33f8ddc15b9be5b8a9e547f3ac302217d97860d
Furkan Çakır <furkancakr7@gmail.com>
2024-05-05 23:01:20 +0300

    3.Commit - 05.05.2024
    Some library changes for convertion jpeg and png

--- conversions/jpegToPng.js
+++ conversions/jpegToPng.js
@@ -1,8 +1,22 @@
-const sharp = require('sharp');
+/*const sharp = require('sharp');
 
 async function jpegToPng(buffer) {
-  console.log('Converting JPEG to PNG...');
   return await sharp(buffer).png().toBuffer();
 }
 
+module.exports = jpegToPng;*/
+
+
+const Jimp = require('jimp');
+
+// Buffer ile JPEG'i PNG'ye dönüştürme fonksiyonu
+async function jpegToPng(buffer) {
+  // Buffer'dan resim yarat
+  const image = await Jimp.read(buffer);
+  // PNG olarak tekrar buffer'a çevir
+  const pngBuffer = await image.getBufferAsync(Jimp.MIME_PNG);
+  return pngBuffer;
+}
+
 module.exports = jpegToPng;
+
--- conversions/pngToJpeg.js
+++ conversions/pngToJpeg.js
@@ -1,7 +1,23 @@
-const sharp = require('sharp');
+/*const sharp = require('sharp');
 
 async function pngToJpeg(buffer) {
   return await sharp(buffer).jpeg().toBuffer();
 }
 
+module.exports = pngToJpeg; */
+
+const Jimp = require('jimp');
+
+// Buffer ile PNG'yi JPEG'e dönüştürme fonksiyonu
+async function pngToJpeg(buffer) {
+  // Buffer'dan resim yarat
+  const image = await Jimp.read(buffer);
+  // JPEG formatına çevir ve kalite ayarla (varsayılan %100 kalite)
+  await image.quality(90); // JPEG kalitesini %90 olarak ayarla
+  // JPEG olarak tekrar buffer'a çevir
+  const jpegBuffer = await image.getBufferAsync(Jimp.MIME_JPEG);
+  return jpegBuffer;
+}
+
 module.exports = pngToJpeg;
+
